6c3760cc37edd270c5f395f7c288ec6445d7e582
Ronit Khurana <ronitkhurana391@gmail.com>
2022-09-14 23:10:54 +0530

    changed from greatest of 3 numbers to greatest of 4 numbers

--- greatestnumber.java
+++ greatestnumber.java
@@ -3,12 +3,12 @@ import java.util.*;
 public class greatestnumber {
     public static void main(String[] args){
         Scanner numbers = new Scanner(System.in);
-        System.out.println("Enter 3 numbers: ");
+        System.out.println("Enter 4 numbers: ");
         int a = numbers.nextInt();
         int b = numbers.nextInt();
         int c = numbers.nextInt();
-
-        System.out.println("The greatest number is: " + Math.max(a, Math.max(b, c)));
+	int d = numbers.nextInt();
+        System.out.println("The greatest number is: " + Math.max(a, Math.max(b, Math.max(c, d))));
     }
 
 }
